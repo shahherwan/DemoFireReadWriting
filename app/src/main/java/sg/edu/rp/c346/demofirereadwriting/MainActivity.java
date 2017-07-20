d3d7d51b27f328a8c6e548630e1c3ee6f668c27a
Herwan Shah <shahherwan@hotmail.com>
2017-07-20 12:01:01 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demofirereadwriting/MainActivity.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.demofirereadwriting;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String folderLocation = Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/Test";
+
+        File folder = new File(folderLocation);
+        if (folder.exists() == false){
+            boolean result = folder.mkdir();
+            if (result == true){
+                Log.d("File Read/Write", "Folder created");
+            }
+        }
+
+    }
+}
